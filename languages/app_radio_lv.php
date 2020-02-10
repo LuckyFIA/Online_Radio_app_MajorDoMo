5dba7b0f3363bac40d9cb8258f1b45db28b4dcdf
BuildTools <unconfigured@null.spigotmc.org>
2020-02-10 16:01:23 +0200

    t3

--- languages/app_radio_lv.php
+++ languages/app_radio_lv.php
@@ -6,7 +6,15 @@
 $dictionary = array(
 /* general */
 'RADIO_NAME' =>'Interneta Rādio',
-'STATION_NAME'=>'Nosaukums'
+'STATION_NAME'=>'Nosaukums',
+'STATION_URL'=>'Raidstacijas URL',
+'ADD_STATION'=>'Pievienot raidstaciju',
+'DEL_STATION'=>'Dzēst Raistaciju',
+'IMPORT_STATION'=>'Importēt raidstaciju',
+'EXPORT_STATION'=>'Eksportēt raidstaciju',
+'SELECT_STATION'=>'Izvēlaties',
+'PLAY_TERMINAL'=>'Atskaņot terminālī'
+
 
 
 /* end module names */
